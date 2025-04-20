e9d88ff1ef3116a2f9fd32b3e8da4ab0d4d5ebab
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-04-20 11:53:57 +0200

    add jsx file, js file

--- src/apiService/photos.js
+++ src/apiService/photos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74';
+const API_KEY = 'PjpInOhywj4qRK8TnuSkTgBPUSFyweK521RxpnGapTDBtl1WeTGcjlXf';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
@@ -9,7 +9,9 @@ axios.defaults.params = {
 };
 
 export const getPhotos = async (query, page) => {
-  const { data } = await axios.get(`search?query=${query}&page=${page}`);
+  const { data } = await axios.get(
+    `https://api.pexels.com/v1/search?query=${query}&page=${page}`
+  );
 
   return data;
 };
